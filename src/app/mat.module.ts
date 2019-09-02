337f0b1295357540b970e3ff96aa1e033102f8aa
Sushmitha R <sushmitha@Sushmithas-MacBook-Air.local>
2019-09-02 23:14:20 +0530

    [initial-setup] initial project setup complete - added angular-mat and bootstrap

---
+++ src/app/mat.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
+  ]
+})
+
+export class MatModule {}
+
